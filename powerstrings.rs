f9b64d72c11d851b23c65498ed94cebdb64b61c2
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2021-01-08 11:23:29 -0800

    optimized it, but it's still too slow
    I need to get some kind of recursive reuse algorithm made, but there are no simple ones :c

--- powerstrings.rs
+++ powerstrings.rs
@@ -1,5 +1,3 @@
-
-
 fn get_string_from_stdin() -> String {
     let mut buffer = String::new();
     std::io::stdin().read_line(&mut buffer).expect("Failed to read line from stdin");
@@ -11,6 +9,79 @@ fn get_string_from_stdin() -> String {
 fn string_repetitions(string: &str) -> u32 {
     let bytes = string.as_bytes();
     let string_len = bytes.len();
+
+    // NOTE: for some strings, string_len/2 can be minimized to string_len/3 and perhaps even further
+    for step in 1..(string_len/2)+1 {
+        if string_len % step == 0 {
+            let mut is_valid = true;
+
+            for i in step..string_len {
+                let head = bytes[i % step];
+                if bytes[i] != head {
+                    is_valid = false;
+                    break;
+                }
+            }
+
+            if is_valid {
+                return (string_len / step) as u32; // how many steps are taken
+            }
+        }
+    }
+
+    1 // means no internal repetitions
+}
+
+fn is_repetitive(bytes: &[u8], step: usize) -> bool {
+    let string_len = bytes.len();
+
+    for i in step..string_len {
+        let head = bytes[i % step];
+        if bytes[i] != head {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn string_repetitions_f(string: &str) -> u32 {
+    let bytes = string.as_bytes();
+    let string_len = bytes.len();
+
+    let mut valid_steps: Vec<usize> = Vec::new();
+    for step in 1..((string_len as f64).sqrt().floor() as usize)+1 {
+        if string_len % step == 0 {
+            valid_steps.push(step);
+            valid_steps.push(string_len / step);
+        }
+    }
+    valid_steps.sort(); // TODO: remove the sort by having two vectors?
+
+    for step in valid_steps {
+        if is_repetitive(bytes, step) {
+            return (string_len / step) as u32; // how many steps are taken
+        }
+    }
+
+    1 // means no internal repetitions
+}
+
+/*
+// breaks the string into duplicate sections of varying sizes -> recursive
+fn string_repetitions_break(string: &str) -> u32 {
+    let bytes = string.as_bytes();
+    let string_len = bytes.len();
+
+    // NOTE: needs this section first
+    let mut valid_steps: Vec<usize> = Vec::new();
+    for step in 1..((string_len as f64).sqrt().floor() as usize) {
+        if string_len % step == 0 {
+            valid_steps.push(step);
+            valid_steps.push(string_len / step);
+        }
+    }
+    valid_steps.sort(); // TODO: remove the sort by having two vectors?
 
     // NOTE: for some strings, string_len/2 can be minimized to string_len/3 and perhaps even further
     for step in 1..(string_len/2)+1 {
@@ -40,76 +111,17 @@ fn string_repetitions(string: &str) -> u32 {
 
     1 // means no internal repetitions
 }
+*/
 
-// TODO: check this.
-fn is_repetitive(bytes: &[u8], step: usize) -> bool {
-    let string_len = bytes.len();
-
-    if string_len % step == 0 {
-        let mut is_valid = true;
-
-        for offset in 0..step { // checks if the string is split into groups of size $split
-            let head = bytes[offset];
-
-            for rep in (step + offset..string_len).step_by(step) {
-                if bytes[rep] != head {  // repetitive pattern is not consistent
-                    is_valid = false;
-                    break;
-                }
-            }
-
-            if !is_valid {
-                break;
-            }
-        }
-
-        if is_valid {
-            return true; // how many steps are taken
-        }
-    }
-
-    return false;
+// This is the top down recursive algorithm.
+fn string_repetitions_reuse(string: &str) -> u32 {
+    0
 }
+
 
 // !!!!!!!!! TRY THIS NEXT !!!!!!!!!!!!
 // TODO: Try an algorithm which recursively builds patterns from the ground up (since patterns are made from the ground up).
 //       this way we only need to do exactly N reads. -> maybe all the sections can communicate through a hashmap-like bus.
-
-// TODO: try this but recursive
-
-// works line binary search
-fn string_repetitions_v2(string: &str) -> u32 {
-    let bytes = string.as_bytes();
-    let string_len = bytes.len();
-
-    // TODO: does this section take too long?
-    // TODO: go to isqrt & add both step and string_len / step
-    let mut valid_steps: Vec<usize> = Vec::new();
-    for step in 1..(string_len/2)+1 {
-        if string_len % step == 0 {
-            valid_steps.push(step);
-        }
-    }
-    valid_steps.push(string_len);
-
-    println!("{:?}", valid_steps);
-
-    let mut beg = 0;
-    let mut end = valid_steps.len() - 1;
-
-    while (end - beg) != 0 {
-        let stepi = (beg + end) / 2;
-        let step = valid_steps[stepi];
-
-        if is_repetitive(bytes, step) {
-            end = stepi; // includes self
-        } else {
-            beg = beg + 1;
-        }
-    }
-
-    (string_len / valid_steps[(beg + end) / 2]) as u32
-}
 
 fn get_input_vector() -> Vec<String> {
     let mut cases: Vec<String> = Vec::new();
@@ -128,6 +140,6 @@ fn main() {
     let cases: Vec<String> = get_input_vector();
 
     for line in cases {
-        println!("{}", string_repetitions_v2(&line));
+        println!("{}", string_repetitions_f(&line));
     }
 }
