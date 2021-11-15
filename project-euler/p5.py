8eec968556119a035463f0be07aeb7c49cd23076
EarthenSkyMobile <24978329+EarthenSky@users.noreply.github.com>
2021-11-15 20:11:47 +0000

    added solution

---
+++ project-euler/p5.py
@@ -0,0 +1,13 @@
+def smallest_evenly_divisible(numbers):
+	#numbers.sort()
+	out = 1
+	for n in numbers:
+		for i in range(0, len(numbers)):
+			if numbers[i] % n == 0:
+				numbers[i] = numbers[i] // n
+		out *= n
+	return out
+
+print(smallest_evenly_divisible( list(range(1,11)) ))
+print(smallest_evenly_divisible( list(range(1,21)) ))
+
