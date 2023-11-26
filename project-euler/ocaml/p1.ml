88b246418543e1805a3e9f156a88aa33efc5b583
Gabe <24978329+EarthenSky@users.noreply.github.com>
2023-11-25 17:10:41 -0800

    clean p1 syntax

--- project-euler/ocaml/p1.ml
+++ project-euler/ocaml/p1.ml
@@ -4,16 +4,20 @@ let print_list list =
   List.iter (Printf.printf "%d ") list;
   Printf.printf "\n"
 
-(* const *)
-let limit = 1000
-
-(* behaviour *)
+let inc = fun x -> x + 1
 
 let any_mod list i =
   List.exists (fun m -> i mod m = 0) list
 
+(* const *)
+
+let limit = 1000
+
+(* behaviour *)
+
 let () = 
-  let nums = (List.init (limit - 1) (fun x -> x + 1)) in
-  let multiples_3_5 = List.filter (any_mod [3; 5]) nums in 
-    print_int (List.fold_left ( + ) 0 multiples_3_5);
+  let nums = List.init (limit - 1) inc in
+  let multiples_of_3_5 = List.filter (any_mod [3; 5]) nums in 
+  let result = List.fold_left ( + ) 0 multiples_of_3_5 in
+    print_int result;
     print_endline ""
