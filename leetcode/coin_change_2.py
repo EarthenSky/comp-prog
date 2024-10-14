68fab2a777d26b8c509555a9d06c130deb0225a2
Gabe <24978329+EarthenSky@users.noreply.github.com>
2024-10-13 22:00:19 -0700

    improve performance of n^2 solution

--- leetcode/coin_change_2.py
+++ leetcode/coin_change_2.py
@@ -1,27 +1,21 @@
 class Solution:
-    # TODO can we do this top down, instead of bottom up, to possibly save some performance?
-    def change(self, amount: int, coins: List[int]) -> int:
+    def change(self, amount: int, coins: list[int]) -> int:
         if amount == 0:
             return 1
         
-        M = [0 for i in range( (len(coins)+1) * (amount+1) )]
+        @lru_cache(maxsize=None)
+        def change_rec(curr_amount, ci):
+            if curr_amount == 0:
+                return 1
+            elif ci == 0:
+                return 0
 
-        # 1 way to use n coins to get 0 dollars (use zero coins)
-        for ci in range(len(coins)+1):
-            M[ci * (amount+1)] = 1
-
-        for ci in range(1, len(coins)+1):
+            curr_index = curr_amount + ci*(amount+1)
+            result = change_rec(curr_amount, ci-1)
             coin = coins[ci-1]
-            for curr_amount in range(1, amount+1):
-                if (curr_amount - coin) == 0:
-                    M[curr_amount + ci*(amount+1)] += (
-                        M[curr_amount - coin + ci*(amount+1)]
-                    )
-                elif (curr_amount - coin) > 0:
-                    M[curr_amount + ci*(amount+1)] += (
-                        M[curr_amount - coin + ci*(amount+1)]
-                    )
+            if curr_amount >= coin:
+                result += change_rec(curr_amount - coin, ci)
 
-                M[curr_amount + ci*(amount+1)] += M[curr_amount + (ci-1)*(amount+1)]
+            return result
 
-        return M[-1]
+        return change_rec(amount, len(coins))
